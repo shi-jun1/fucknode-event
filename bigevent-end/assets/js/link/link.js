0c2830d6e278fb3a0a795455c030c3ef322d6354
liulibin <liulibin@itcast.cn>
2021-05-12 11:44:37 +0800

    友情链接功能完成

---
+++ bigevent-end/assets/js/link/link.js
@@ -0,0 +1,158 @@
+$(function() {
+    friendList()
+
+    function friendList() {
+
+        $.ajax({
+            method: 'get',
+            url: '/admin/links',
+            success: function(res) {
+                var htmlStr = template('tpl-friend', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+    //添加==============================================================
+    var indexA = null
+    $('#add-link').on('click', function() {
+            indexA = layui.layer.open({
+                type: 1,
+                area: ['500px', '330px'],
+                title: '添加友情链接',
+                content: $('#add-form-tpl').html()
+            })
+
+            $('#urlIcon').on('click', function() {
+                $('#linkFile').click()
+
+
+            })
+            $('#linkFile').on('change', function(e) {
+                //获取用户选择的文件
+                var filelist = e.target.files
+                if (filelist.length === 0) {
+                    return layui.layer.msg('请选择照片！')
+                }
+
+                var file = e.target.files[0]
+                var imgURL = URL.createObjectURL(file)
+                $('#preIcon').attr('src', imgURL)
+
+                $('body').on('submit', '#add-form', function(e) {
+                    e.preventDefault()
+
+                    var fd = new FormData(this)
+                    $.ajax({
+                        method: 'post',
+                        url: '/admin/links',
+                        data: fd,
+                        contentType: false,
+                        processData: false,
+                        success: function(res) {
+                            if (res.status !== 0) {
+                                return layui.layer.msg(res.message, { icon: 5 })
+                            }
+                            friendList()
+                            layui.layer.close(indexA)
+                            layui.layer.msg('添加成功!', { icon: 6 }, function() {
+
+                            })
+                        }
+                    })
+                })
+            })
+        })
+        //编辑===========================================================
+    $('body').on('click', '#bianJi', function() {
+            layui.layer.open({
+                type: 1,
+                area: ['500px', '330px'],
+                title: '编辑友情链接',
+                content: $('#edit-form-tpl').html()
+            })
+
+            var id = $(this).attr('data_id')
+
+            $.ajax({
+                method: 'get',
+                url: '/admin/links/ ' + id,
+                success: function(res) {
+                    // console.log(res);
+                    layui.form.val('editForm', res.data)
+
+
+
+                }
+
+            })
+
+            var date;
+            $('#urlIcon').on('click', function() {
+                $('#linkFile').click()
+                $('#linkFile').on('change', function(e) {
+                    //获取用户选择的文件
+                    var filelist = e.target.files
+                    if (filelist.length === 0) {
+                        return layui.layer.msg('请选择照片！')
+                    }
+                    date = filelist[0]
+                    var file = e.target.files[0]
+                    var imgURL = URL.createObjectURL(file)
+                    $('#preIcon').attr('src', imgURL)
+
+                })
+
+            })
+
+            $('body').on('submit', '#edit-form', function(e) {
+                e.preventDefault()
+
+                var fd = new FormData(this)
+                fd.append('linkicon', date)
+                    // fd.forEach((v, i) => {
+                    //     console.log(i, v);
+                    // })
+                $.ajax({
+                    method: 'put',
+                    url: '/admin/links/' + id,
+                    data: fd,
+                    contentType: false,
+                    processData: false,
+                    success: function(res) {
+
+                        if (res.status !== 0) {
+                            return layui.layer.msg(res.message, { icon: 5 })
+                        }
+                        friendList()
+                        layui.layer.close(indexA)
+                        layui.layer.msg(res.message, { icon: 6 }, function() {
+
+                        })
+                    }
+                })
+            })
+        })
+        //删除==============================================================
+
+    $('body').on('click', '#shanChu', function() {
+        var id = $(this).attr('delete_id')
+        layer.confirm('确定删除', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                method: 'delete',
+                url: '/admin/links/' + id,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layui.layui.msg(res.message, { icon: 5 })
+                    }
+                    layui.layer.msg(res.message, { icon: 6 })
+                    layer.close(index);
+                    friendList()
+                }
+            })
+
+
+        });
+    })
+
+
+})
--- bigevent-front/js/common.js
+++ bigevent-front/js/common.js
@@ -1,8 +1,24 @@
+$(function() {
+    // 菜单展开/折叠交互
+    $('.menus .triangle').click(function() {
+        $(this).parents('li').toggleClass('collapsed');
+    })
 
-$(function () {
-  // 菜单展开/折叠交互
-  $('.menus .triangle').click(function () {
-    $(this).parents('li').toggleClass('collapsed');
-  })
 
+
+
+    fn()
+
+    function fn() {
+        $.ajax({
+            method: 'get',
+            url: '/api/links',
+            success: function(res) {
+                // console.log(res);
+                var htmlStr = template('tpl-common', res)
+                $('.kr_collaborator').html(htmlStr)
+            }
+        })
+
+    }
 })
