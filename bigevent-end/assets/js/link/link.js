890c835332c3cec22efcaa403ad8fc5c16b2e391
liulibin <liulibin@itcast.cn>
2021-05-12 21:41:04 +0800

    完善版link

--- bigevent-end/assets/js/link/link.js
+++ bigevent-end/assets/js/link/link.js
@@ -9,62 +9,66 @@ $(function() {
             success: function(res) {
                 var htmlStr = template('tpl-friend', res)
                 $('tbody').html(htmlStr)
+                console.log(res);
             }
         })
     }
     //添加==============================================================
-    var indexA = null
+    var index = null
+
     $('#add-link').on('click', function() {
-            indexA = layui.layer.open({
-                type: 1,
-                area: ['500px', '330px'],
-                title: '添加友情链接',
-                content: $('#add-form-tpl').html()
-            })
+        index = layui.layer.open({
+            type: 1,
+            area: ['500px', '330px'],
+            title: '添加友情链接',
+            content: $('#add-form-tpl').html()
+        })
 
-            $('#urlIcon').on('click', function() {
-                $('#linkFile').click()
+        $('#urlIcon').on('click', function() {
+            $('#linkFile').click()
 
 
-            })
-            $('#linkFile').on('change', function(e) {
-                //获取用户选择的文件
-                var filelist = e.target.files
-                if (filelist.length === 0) {
-                    return layui.layer.msg('请选择照片！')
+        })
+        $('#linkFile').on('change', function(e) {
+            //获取用户选择的文件
+            var filelist = e.target.files
+            if (filelist.length === 0) {
+                return layui.layer.msg('请选择照片！')
+            }
+
+            var file = e.target.files[0]
+            var imgURL = URL.createObjectURL(file)
+            $('#preIcon').attr('src', imgURL)
+
+
+        })
+    })
+    $('body').on('submit', '#add-form', function(e) {
+            e.preventDefault()
+
+            var fd = new FormData(this)
+            $.ajax({
+                method: 'post',
+                url: '/admin/links',
+                data: fd,
+                contentType: false,
+                processData: false,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layui.layer.msg(res.message, { icon: 5 })
+                    }
+
+
+                    layui.layer.msg('添加成功!', { icon: 6 })
+                    friendList()
+                    layui.layer.close(index)
+
                 }
-
-                var file = e.target.files[0]
-                var imgURL = URL.createObjectURL(file)
-                $('#preIcon').attr('src', imgURL)
-
-                $('body').on('submit', '#add-form', function(e) {
-                    e.preventDefault()
-
-                    var fd = new FormData(this)
-                    $.ajax({
-                        method: 'post',
-                        url: '/admin/links',
-                        data: fd,
-                        contentType: false,
-                        processData: false,
-                        success: function(res) {
-                            if (res.status !== 0) {
-                                return layui.layer.msg(res.message, { icon: 5 })
-                            }
-                            friendList()
-                            layui.layer.close(indexA)
-                            layui.layer.msg('添加成功!', { icon: 6 }, function() {
-
-                            })
-                        }
-                    })
-                })
             })
         })
         //编辑===========================================================
     $('body').on('click', '#bianJi', function() {
-            layui.layer.open({
+            index = layui.layer.open({
                 type: 1,
                 area: ['500px', '330px'],
                 title: '编辑友情链接',
@@ -79,9 +83,6 @@ $(function() {
                 success: function(res) {
                     // console.log(res);
                     layui.form.val('editForm', res.data)
-
-
-
                 }
 
             })
@@ -123,11 +124,12 @@ $(function() {
                         if (res.status !== 0) {
                             return layui.layer.msg(res.message, { icon: 5 })
                         }
-                        friendList()
-                        layui.layer.close(indexA)
-                        layui.layer.msg(res.message, { icon: 6 }, function() {
 
-                        })
+
+                        layui.layer.msg(res.message, { icon: 6 })
+                        friendList()
+                        layui.layer.close(index)
+
                     }
                 })
             })
@@ -136,7 +138,7 @@ $(function() {
 
     $('body').on('click', '#shanChu', function() {
         var id = $(this).attr('delete_id')
-        layer.confirm('确定删除', { icon: 3, title: '提示' }, function(index) {
+        layer.confirm('确定删除', { icon: 3, title: '提示' }, function(indexA) {
             $.ajax({
                 method: 'delete',
                 url: '/admin/links/' + id,
@@ -145,7 +147,7 @@ $(function() {
                         return layui.layui.msg(res.message, { icon: 5 })
                     }
                     layui.layer.msg(res.message, { icon: 6 })
-                    layer.close(index);
+                    layui.layer.close(index);
                     friendList()
                 }
             })
